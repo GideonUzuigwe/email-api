b91bcb18377df888d4633f27b5e12169bc286345
Uzuigwe Gideon <uptodateacademy@outlook.com>
2024-09-30 23:15:41 +0100

    simple email api

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const PORT = process.env.port || 3000;
+
+//Initialize the environmental variable
+dotenv.config();
+
+//List all the middlewares you'll use in this project;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//POST
+app.post("/send-mail", async (req, res) => {
+    const { to, name, subject, message } = req.body;
+
+    //Create a transport;
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    })
+
+    //Email Options
+    const emailOptions = {
+        from: process.env.EMAIL_USER,
+        to,
+        name,
+        subject,
+        message
+    }
+
+    try {
+        await transporter.sendMail(emailOptions);
+        res.status(200).send('Email sent successfully');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error sending email');
+    }
+})
+
+app.listen(PORT, console.log("app is live"))
