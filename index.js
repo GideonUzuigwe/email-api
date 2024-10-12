19db3c027311497843f2b1f404117d4e70cda116
Uzuigwe Gideon <uptodateacademy@outlook.com>
2024-10-13 00:15:32 +0100

    Email API Production

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const express = require("express");
 const app = express();
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 //Initialize the environmental variable
 dotenv.config();
 
 //List all the middlewares you'll use in this project;
-app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("dist"))
+
 //POST
-app.post("/send-mail", async (req, res) => {
+app.post("/api/send-mail", async (req, res) => {
     const { email, subject, message } = req.body;
 
     //Create a transport;
@@ -40,6 +40,6 @@ app.post("/send-mail", async (req, res) => {
     } catch (error) {
         res.status(500).send({ "message": 'Error sending email' });
     }
-})
+});
 
-app.listen(PORT, console.log("app is live"))
+app.listen(PORT, console.log(`Our Server is life @ port ${PORT}`))
